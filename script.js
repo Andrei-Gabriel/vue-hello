ae55e3b94625aa6025be083ba40ac09756ad17b7
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2021-11-25 19:04:34 +0100

    FIne es

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello World");
+// CONSEGNA
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+
+// Bonus:
+    // Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const app = new Vue({
+    el: '#root',
+    data:{
+        message: "Titolo della pagina",
+        img: "https://picsum.photos/200"
+    }
+});
